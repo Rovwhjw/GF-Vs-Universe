858c4f165538b831f6ed6831bc51af5ed0935412
Rovwhjw <96930237+Rovwhjw@users.noreply.github.com>
2022-01-20 19:36:59 +0300

    Add files via upload

---
+++ assets/preload/custom_events/death.lua
@@ -0,0 +1,4 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'GF_Player'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'deathGF'); --put in mods/sounds/
+end
---
+++ assets/preload/custom_events/deathBF.lua
@@ -0,0 +1,3 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'GoldenBF'); --Character json file for the death animation
+end
